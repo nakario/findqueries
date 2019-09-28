3ee73ba3efc5c42257e50214706ca6d13d1bd402
nakario <nakario.jp@gmail.com>
2019-09-28 21:07:17 +0900

    Update test codes

--- test/a.go
+++ test/a.go
@@ -1,8 +1,15 @@
 package a
 
 import (
+	"fmt"
+
 	"github.com/nakario/findqueries/test/b"
 )
+
+func callHoge() {
+	f := hoge()
+	fmt.Println(f.GetPiyo())
+}
 
 func hoge() *b.Fuga {
 	return b.NewFuga()
--- test/b/b.go
+++ test/b/b.go
@@ -5,6 +5,11 @@ type Fuga struct {
 	Piyo string
 }
 
+// GetPiyo returns f.Piyo
+func (f *Fuga) GetPiyo() string {
+	return f.Piyo
+}
+
 // NewFuga returns a new Fuga
 func NewFuga() *Fuga {
 	return &Fuga{"piyo"}
