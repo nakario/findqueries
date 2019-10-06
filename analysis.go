d4cd543eb33511a81abb6355e56487c55eeacaf5
nakario <nakario.jp@gmail.com>
2019-10-07 01:45:54 +0900

    Unify analysis loops

--- analysis.go
+++ analysis.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"go/ast"
+	"go/token"
+	"go/types"
+
 	"github.com/pkg/errors"
 	"golang.org/x/tools/go/packages"
 	"golang.org/x/tools/go/ssa/ssautil"
@@ -39,21 +43,31 @@ func analyze(dir string, queryers []queryerInfo) (*result, error) {
 		return nil, errors.WithMessagef(err, "failed to parse dir %s", dir)
 	}
 
-	queries := make(map[string][]queryInfo)
+	pkg2pos2expr := make(map[*types.Package]map[token.Pos]*ast.CallExpr)
+	for _, p := range pkgs {
+		pos2expr := make(map[token.Pos]*ast.CallExpr)
+		for _, file := range p.Syntax {
+			ast.Inspect(file, func(n ast.Node) bool {
+				if ce, ok := n.(*ast.CallExpr); ok {
+					pos2expr[ce.Lparen] = ce
+				}
+				return true
+			})
+		}
+		pkg2pos2expr[p.Types] = pos2expr
+	}
 
-	for _, pkg := range pkgs {
-		qs, err := findQueries(pkg, queryers)
+	queries := make(map[string][]queryInfo)
+	calls := make(map[string][]call)
+
+	_, ssaPkgs := ssautil.Packages(pkgs, 0)
+	for _, pkg := range ssaPkgs {
+		if pkg == nil { continue }
+		qs, cs, err := findCalls(pkg, queryers, pkg2pos2expr[pkg.Pkg])
 		if err != nil {
 			return nil, err
 		}
-		queries[pkg.Name] = qs
-	}
-
-	_, ssaPkgs := ssautil.Packages(pkgs, 0)
-	calls := make(map[string][]call)
-	for _, pkg := range ssaPkgs {
-		if pkg == nil { continue }
-		cs := findCalls(pkg)
+		queries[pkg.Pkg.Name()] = qs
 		calls[pkg.Pkg.Name()] = cs
 	}
 	
--- calls.go
+++ calls.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"go/ast"
+	"go/token"
+	"go/types"
+	"os"
 	"sort"
+	"strconv"
 
+	"github.com/pkg/errors"
 	"golang.org/x/tools/go/callgraph/cha"
 	"golang.org/x/tools/go/ssa"
 )
 
-func findCalls(pkg *ssa.Package) []call {
+func findCalls(pkg *ssa.Package, queryers []queryerInfo, pos2expr map[token.Pos]*ast.CallExpr) ([]queryInfo, []call, error) {
+	queryersMap := make(map[string]int)
+	for _, qi := range queryers {
+		queryersMap[qi.FullName] = qi.QueryPos
+	}
+	queries := make([]queryInfo, 0)
+	unresolved := make([]queryInfo, 0)
 	er2ees := make(map[string][]string)
 	pkg.Build()
 	cg := cha.CallGraph(pkg.Prog)
@@ -19,8 +32,44 @@ func findCalls(pkg *ssa.Package) []call {
 		for _, e := range node.Out {
 			caller := e.Caller.Func.RelString(pkg.Pkg)
 			callee := e.Callee.Func.RelString(pkg.Pkg)
+
+			if pos, ok := queryersMap[callee]; ok {
+				site := e.Site
+				if site != nil {
+					if site.Pos() == token.NoPos {
+						return nil, nil, errors.New("unexpectedly a call doesn't have its position")
+					}
+					qi := queryInfo{Caller: caller, Pos: pkg.Prog.Fset.Position(site.Pos()).String()}
+					if expr, ok := pos2expr[site.Pos()]; ok {
+						qi.Expr = types.ExprString(expr)
+					} else {
+						return nil, nil, errors.New("found an unexpected call")
+					}
+					args := site.Common().Args
+					if site.Common().Signature().Recv() != nil {
+						args = args[1:]
+					}
+					query := args[pos]
+					if query, ok := query.(*ssa.Const); ok {
+						query, _ := strconv.Unquote(query.Value.ExactString())
+						qi.Query = query
+						queries = append(queries, qi)
+					} else {
+						unresolved = append(unresolved, qi)
+					}
+				} else {
+					fmt.Println("unexpected synthetic or intrinsic call")
+				}
+			}
 			er2ees[caller] = append(er2ees[caller], callee)
 		}
+	}
+
+	if len(unresolved) > 0 {
+		fmt.Fprintln(os.Stderr, "UNRESOLVED")
+	}
+	for _, qi := range unresolved {
+		fmt.Fprintln(os.Stderr, qi.Pos, qi.Expr)
 	}
 
 	ers := make([]string, 0, len(er2ees))
@@ -38,5 +87,5 @@ func findCalls(pkg *ssa.Package) []call {
 		}
 	}
 
-	return calls
+	return queries, calls, nil
 }
